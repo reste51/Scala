6f28310918734e14390444224c24228f54d8c35a
reste51 <61609268+reste51@users.noreply.github.com>
2020-06-12 17:39:23 +0800

    Scala_ spark 访问 hadoop 文件

--- spark-maven-test/src/main/java/com/test/spark/First.scala
+++ spark-maven-test/src/main/java/com/test/spark/First.scala
@@ -4,7 +4,7 @@ import org.apache.spark.{SparkConf, SparkContext}
   * @author Yezhiwei
   */
 
-object WordCount {
+object First {
   def main (args: Array[String]){
 
     //注：  设置 hadoop 的home目录, 便于访问 winutils 工具
@@ -12,7 +12,7 @@ object WordCount {
 
     System.setProperty("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
-    val conf = new SparkConf().setMaster("spark://192.168.136.130:7077").setAppName("WC")
+    val conf = new SparkConf().setMaster("spark://192.168.136.130:7077").setAppName("TestMaven")
     val sc = new SparkContext(conf)
 
     val rdd = sc.textFile("hdfs://192.168.136.130/mp/hello.txt")
@@ -20,15 +20,15 @@ object WordCount {
     print("总数为：  "+rdd.count())
 
     // First item in this RDD
-    val firstText = rdd.first()
-    print("第一个值： " + firstText)
+//    val firstText = rdd.first()
+//    print("第一个值： " + firstText)
 
     /**
       * We will use the filter transformation to return a new RDD with a subset of the items in the file.
       *  How many lines contain "hello"?
-       */
-//      val count = rdd.filter(line => line.contains("hello")).count()
-//      print("How many lines contain \"hello\"?" + count)
+      */
+//    val count = rdd.filter(line => line.contains("hello")).count()
+//    print("How many lines contain \"hello\"?" + count)
     sc.stop()
   }
 }
