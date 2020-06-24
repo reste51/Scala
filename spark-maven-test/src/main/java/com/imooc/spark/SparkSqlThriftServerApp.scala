fd0950fb99c75db751263aac95d7a02256d7718e
reste51 <61609268+reste51@users.noreply.github.com>
2020-06-24 09:08:26 +0800

    使用jdbc 连接 ThriftServer 访问 hive 数据

---
+++ spark-maven-test/src/main/java/com/imooc/spark/SparkSqlThriftServerApp.scala
@@ -0,0 +1,30 @@
+package com.imooc.spark
+
+import java.sql.DriverManager
+
+/**
+  * 通过 Jdbc的方式连接 ThriftServer
+  */
+object SparkSqlThriftServerApp {
+  def main(args: Array[String]): Unit = {
+
+    // 注: 加载 hive的jdbc驱动
+    Class.forName("org.apache.hive.jdbc.HiveDriver")
+
+    // 连接 spark的 thriftServer
+    val connection = DriverManager.getConnection("jdbc:hive2://192.168.136.130:10000","hadoop","hadoop")
+
+    // 操作sql
+    val pre = connection.prepareStatement(" select empno,ename,job from emp")
+    val resultSet = pre.executeQuery()
+
+    while (resultSet.next()){
+      println("empno:" + resultSet.getInt("empno") +", ename : "
+        + resultSet.getString("ename") + ", job : " + resultSet.getString("job"))
+    }
+    resultSet.close()
+    pre.close()
+    connection.close()
+
+  }
+}
