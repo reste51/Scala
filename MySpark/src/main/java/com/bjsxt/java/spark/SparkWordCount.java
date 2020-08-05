ed9b703cae281b73f5a5aa1b98a9084106681a6e
reste51 <61609268+reste51@users.noreply.github.com>
2020-08-05 14:22:23 +0800

    sxt_spark启动

---
+++ MySpark/src/main/java/com/bjsxt/java/spark/SparkWordCount.java
@@ -0,0 +1,56 @@
+package com.bjsxt.java.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class SparkWordCount {
+    public static void main(String[] args) {
+
+        //注：  设置 hadoop 的home目录, 便于访问 winutils 工具
+        System.setProperty("hadoop.home.dir", "D:\\program_files\\winutils\\winutils-master\\hadoop-2.6.3");
+
+
+        SparkConf conf = new SparkConf();
+        conf.setMaster("local");
+        conf.setAppName("wc");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> lines = sc.textFile("./data/words");
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterator<String> call(String line) throws Exception {
+                return Arrays.asList(line.split(" ")).iterator();
+            }
+        });
+        JavaPairRDD<String, Integer> pairWords = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2<>(s, 1);
+            }
+        });
+        JavaPairRDD<String, Integer> result = pairWords.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+        result.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+            @Override
+            public void call(Tuple2<String, Integer> tp) throws Exception {
+                System.out.println();
+            }
+        });
+        sc.stop();
+
+
+    }
+}
---
+++ MySpark/src/main/java/com/bjsxt/scala/spark/SparkWC.scala
@@ -0,0 +1,40 @@
+package com.bjsxt.scala.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * spark wordcount
+  */
+object SparkWC {
+  def main(args: Array[String]): Unit = {
+
+    //注：  设置 hadoop 的home目录, 便于访问 winutils 工具
+    System.setProperty("hadoop.home.dir", "D:\\program_files\\winutils\\winutils-master\\hadoop-2.6.3");
+
+    val conf = new SparkConf().setAppName("wordcount").setMaster("local")
+    val sc = new SparkContext(conf)
+    sc.textFile("./data/words").flatMap( _.split(" ")).map((_,1)).reduceByKey(_+_).foreach(println)
+    sc.stop()
+
+
+
+
+//    //conf 可以设置SparkApplication 的名称，设置Spark 运行的模式
+//    val conf = new SparkConf()
+//    conf.setAppName("wordcount")
+//    conf.setMaster("local")
+//    //SparkContext 是通往spark 集群的唯一通道
+//    val sc = new SparkContext(conf)
+//
+//    val lines: RDD[String] = sc.textFile("./data/words")
+//    val words: RDD[String] = lines.flatMap(line => {
+//      line.split(" ")
+//    })
+//    val pairWords: RDD[(String, Int)] = words.map(word=>{new Tuple2(word,1)})
+//    val result: RDD[(String, Int)] = pairWords.reduceByKey((v1:Int, v2:Int)=>{v1+v2})
+//    result.foreach(one=>{
+//      println(one)
+//    })
+
+  }
+}
