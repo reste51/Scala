1fad171465a46b0c80dbbae6b76d32415859663e
reste51 <61609268+reste51@users.noreply.github.com>
2020-08-10 09:32:39 +0800

    spark 算子 operator 的transform, action操作

---
+++ MySpark/src/main/java/com/bjsxt/scala/spark/PersistentDemo.scala
@@ -0,0 +1,30 @@
+package com.bjsxt.scala.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * 持久化算子
+  */
+object PersistentDemo {
+  def main(args: Array[String]): Unit = {
+
+    System.setProperty("hadoop.home.dir", "D:\\program_files\\winutils\\winutils-master\\hadoop-2.6.3");
+
+
+    val conf = new SparkConf();
+    conf.setAppName("persistentDemo")
+    conf.setMaster("local")
+
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("./data/words")
+    val errorsRdd: RDD[String] = lines.filter(_.startsWith("ERROR"))
+
+    val errors = errorsRdd.filter(_.contains("MYSQL")).count()
+    val errors2 = errorsRdd.filter(_.contains("Http")).count()
+
+
+
+  }
+}
