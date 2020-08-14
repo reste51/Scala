2aa4939146cbbe63d52cb8d80afffd05904135b7
reste51 <61609268+reste51@users.noreply.github.com>
2020-08-14 19:01:58 +0800

    spark 算子 operator 的transform, action操作

---
+++ MySpark/src/main/java/com/bjsxt/scala/spark/DistinctOperator.scala
@@ -0,0 +1,27 @@
+package com.bjsxt.scala.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * distinct 算子的demo
+  * 产生rdd的方法：
+  * sc.textFile("")
+  */
+object DistinctOperator {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setMaster("local").setAppName("DistinctOperator")
+    val sc = new SparkContext(conf)
+
+
+    val rdd: RDD[String] = sc.parallelize(List[String]("1","hello","hello","hello","word","word","wo"))
+    // wordCount思路写distinct : foreach 为action 触发 transform 执行
+    rdd.map(word=>(word,1)).reduceByKey((val1,val2)=> val1+val2).map(_._1).foreach(println)
+
+    // 源码:     map(x => (x, null)).reduceByKey((x, y) => x, numPartitions).map(_._1)
+//    rdd.distinct().foreach(println)
+
+    sc.stop()
+  }
+}
