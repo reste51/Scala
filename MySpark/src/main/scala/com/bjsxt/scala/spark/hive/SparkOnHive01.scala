0f41c076ee163d4ba7963560b177028cd5b16c0b
reste51 <61609268+reste51@users.noreply.github.com>
2020-09-22 11:02:32 +0800

    spark sql 连接 hive 查询数据

--- MySpark/src/main/scala/com/bjsxt/scala/spark/hive/SparkOnHive01.scala
+++ MySpark/src/main/scala/com/bjsxt/scala/spark/hive/SparkOnHive01.scala
@@ -5,6 +5,14 @@ import org.apache.spark.sql.{SaveMode, SparkSession}
 
 /***
   * spark sql 连接 hive 查询数据
+
+  hive-site.xml 修正
+  hive.metastore.uris：sparksql 连接到这里，这里是hive的metastore，用于获取hive表; 另一个是禁用metastore的版本检测
+  4.开启hive的metastore元数据库
+  spark sql想要使用hive的表，还需要hive开启metastore
+  hive --service metastore &
+  启动后放后台就可以，供spark sql使用
+
   */
 object SparkOnHive01 {
 
@@ -14,11 +22,15 @@ object SparkOnHive01 {
 
     sparkSession.sql("show databases").show(20)
 //    sparkSession.sql("use spark ")
+
+    /*
+     注: 使用以下 不能保存， 具体原因不太懂
+     */
     import sparkSession.implicits._
     // 创建一个dataSets
     val caseClassDS = Seq(Person("Andy",22),Person("Tomcat",12),Person("From",42),Person("Tidy",32)).toDF()
-    caseClassDS.write.option("spark.sql.hive.convertMetastoreParquet", false).mode(SaveMode.Append).saveAsTable("kof")
-
+    // option("spark.sql.hive.convertMetastoreParquet", false).
+    caseClassDS.write.mode(SaveMode.Append).saveAsTable("kof")
 
     sparkSession.stop()
   }
---
+++ MySpark/src/main/scala/com/bjsxt/scala/spark/hive/SparkOnHive2.scala
@@ -0,0 +1,52 @@
+package com.bjsxt.scala.spark.hive
+
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+
+/**
+  * 使用sql访问 hive， 并创建 表 和 加载数据/  存储表。
+
+
+    hive-site.xml 修正
+  hive.metastore.uris：sparksql 连接到这里，这里是hive的metastore，用于获取hive表; 另一个是禁用metastore的版本检测
+
+4.开启hive的metastore元数据库
+  spark sql想要使用hive的表，还需要hive开启metastore
+  hive --service metastore &
+  启动后放后台就可以，供spark sql使用
+
+  */
+object SparkOnHive2 {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("SparkOnHive2").enableHiveSupport().getOrCreate()
+
+    // 隐式导入
+
+    spark.sql("create table if not exists src(key int, value string)")
+    spark.sql("load data local inpath '/home/hadoop/app/spark-2.1.0-bin-2.6.0-cdh5.7.0/examples/src/main/resources/kv1.txt' into table src")
+
+    // Queries are expressed in HiveQL
+    spark.sql("select * from src").show(20)
+
+    // Aggregation queries are also supported.
+    spark.sql("select count(*) from src").show()
+
+    // The results of SQL queries are themselves DataFrames and support all normal functions.
+    val sqlDF = spark.sql("SELECT key, value FROM src WHERE key < 10 ORDER BY key")
+
+    import  spark.implicits._
+
+    // The items in DaraFrames are of type Row, which allows you to access each column by ordinal.
+    val stringDS = sqlDF.map{
+      case Row(key:Int, value:String)=> s"the key is $key, and the value is $value"
+    }
+    stringDS.show(200,false)
+    //save table
+    stringDS.write.mode(SaveMode.Overwrite).saveAsTable("src_ret")
+
+
+    // You can also use DataFrames to create temporary views within a SparkSession.
+//    val recordsDF= spark.createDataFrame((1 to 100).map(i=> Record)
+
+
+  }
+}
